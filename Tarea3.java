a4f7509b229370b782f33f6272ecb455d19879a6
xleoh <124121327@morelia.tecnm.mx>
2024-09-11 23:03:50 -0600

    tarea3

---
+++ Tarea3.java
@@ -0,0 +1,45 @@
+public class Main {
+    public static void main(String[] args) {
+        //informacion de un libro
+        String Titulo = "1984";
+        String Autor = "george orwell";
+        String GeneroLiterario = "distopía";
+        int numeroDePaginas = 328;
+
+        System.out.println(Titulo);
+        System.out.println(Autor);
+        System.out.println(GeneroLiterario);
+        System.out.println(numeroDePaginas);
+
+
+
+        //Informacion de un pais que deseo visitar
+        String Pais = "Francia";
+        String Capital = "Paris";
+        String idiomaOficial = "Frances";
+        double numeroDeHabitantes = 65354806;
+        String monedaLocal = "Euro";
+
+        System.out.println(Pais);
+        System.out.println(Capital);
+        System.out.println(idiomaOficial);
+        System.out.println("Poblacion" + (numeroDeHabitantes / 1000000)+'M');
+        System.out.println(monedaLocal);
+
+
+
+        // Informacion de mi personaje historico favorito
+
+        String NombreCompleto = "Carroll Shelby";
+        String FechaDeNacimiento = "11 de enero de 1923";
+        String Nacionalidad = "Estado Unidense";
+        String Ocupacion = "mecanico";
+        String logroDestacado = "Ganar Lemans";
+
+        System.out.println(NombreCompleto);
+        System.out.println(FechaDeNacimiento);
+        System.out.println(Nacionalidad);
+        System.out.println(Ocupacion);
+        System.out.println(logroDestacado);
+    }
+}
