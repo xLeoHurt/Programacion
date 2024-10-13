3df16af6f7aae4deec97cb1c2d0d39a94dedc2e9
xLeoHurt <leohurt06@gmail.com>
2024-10-12 22:03:45 -0600

    Tarea3-Unidad3

---
+++ Main.java
@@ -0,0 +1,145 @@
+public class Main {
+    public static void main(String[] args) {
+        //problema 1
+        if ((3 < 5 && 7 >= 2)==true){
+            System.out.println("El resultado de (3 < 5 && 7 >= 2 es VERDADERO");
+        } else {
+            System.out.println("El resultado de (3 < 5 && 7 >= 2 es FALSO");
+        }
+
+        //problema 2
+        if ((10!=8||15==15) == true) {
+            System.out.println("El resultado del problema 10!=8||15==15 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema 10!=8||15==15 es FALSO");
+        }
+
+        //problema 3
+        if ((!(5>6)&&2<=3) == true){
+            System.out.println("El resultado del problema !(5>6)&&2<=3 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema !(5>6)&&2<=3 es FALSO");
+        }
+
+        //PROBLEMA 4
+        if (((8==8||3<1)&&6>2)==true){
+            System.out.println("El resultado del problema 8==8||3<1) && 6>2 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema 8==8||3<1) &&6> 2 es FALSO ");
+        }
+
+        //PROBLEMA 5
+        if ((12 != 10 && (4 < 8 || 9 == 9)==true)){
+            System.out.println("El resultado del problema 12 != 10 && (4 < 8 || 9 == 9 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema 12 != 10 && (4 < 8 || 9 == 9 es FALSO)");
+        }
+
+        //problema 6
+        if (((3 + 5) == 8|| (7 * 2) <= 14) == true){
+            System.out.println("El resultado del problema (3 + 5) == 8|| (7 * 2) <= 14 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema (3 + 5) == 8|| (7 * 2) <= 14 es FALSO");
+        }
+
+        //problema 7
+        if ((!(9 <= 5) && (4 == 4 || 6 != 6))==true){
+            System.out.println("El resultado del problema (9 <= 5) && (4 == 4 || 6 != 6) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema (9 <= 5) && (4 == 4 || 6 != 6) es FALSO");
+        }
+
+        //problema 8
+        if ((10 % 2 == 0 && (5 > 1 || 12 != 10))== true){
+            System.out.println("El resultado del problema 10 % 2 == 0 && (5 > 1 || 12 != 10) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema 10 % 2 == 0 && (5 > 1 || 12 != 10) Falso");
+        }
+
+        //problema 9
+        if (((3 * 3 == 9) && ! (8<5 || 7 >= 9))==true){
+            System.out.println("El resultado del problema (3 * 3 == 9) && ! (8<5 || 7 >= 9) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema (3 * 3 == 9) && ! (8<5 || 7 >= 9) es FALSO");
+        }
+
+        //problema 10
+        if (( (5 + 5 != 10) && (2 * 3 > 5 || 6 == 7))==true){
+            System.out.println("El resultado del problema (5 + 5 != 10) && (2 * 3 > 5 || 6 == 7) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema  (5 + 5 != 10) && (2 * 3 > 5 || 6 == 7) es FALSO");
+        }
+
+        //problema 11
+        if ((20 / 4 == 5|| !(10 % 3 == 1 && 4 >= 3))==true){
+            System.out.println("El resultado del problema 20 / 4 == 5|| !(10 % 3 == 1 && 4 >= 3 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema 20 / 4 == 5|| !(10 % 3 == 1 && 4 >= 3 es FALSO");
+        }
+
+        //Problema 12
+        if ((! (7 > 3 && 2 == 2) || (5 * 2 < 12 && 8 >= 8))==true){
+            System.out.println("El resultado del problema ! (7 > 3 && 2 == 2) || (5 * 2 < 12 && 8 >= 8) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema ! (7 > 3 && 2 == 2) || (5 * 2 < 12 && 8 >= 8) es FALSO");
+        }
+
+        //problema 13
+        if (((15 - 5 > 8) &&! (3 == 3 || 7 < 1) && 12 >= 10)==true){
+            System.out.println("El resultado del problema (15 - 5 > 8) &&! (3 == 3 || 7 < 1) && 12 >= 10 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema (15 - 5 > 8) &&! (3 == 3 || 7 < 1) && 12 >= 10 es FALSO");
+        }
+
+        //problema 14
+        if ((9 % 3 == 0 && (8 <= 12 || 5 + 5 != 10) && ! (7 > 8))==true){
+            System.out.println("El resultado del problema 9 % 3 == 0 && (8 <= 12 || 5 + 5 != 10) && ! (7 > 8) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema 9 % 3 == 0 && (8 <= 12 || 5 + 5 != 10) && ! (7 > 8) es FALSO");
+        }
+
+        //PROBLEMA 15
+        if ((!(10 != 10) && (5 * 3 == 15 || 2 < 1) && 9 >= 9)==true){
+            System.out.println("El resultado del problema !(10 != 10) && (5 * 3 == 15 || 2 < 1) && 9 >= 9 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema !(10 != 10) && (5 * 3 == 15 || 2 < 1) && 9 >= 9 es FALSO");
+        }
+
+        //Problema 16
+        if (((4 + 6 == 10 || 2 == 3) &&! (8 < 8 && 3>2) || 7% 2 == 1)==true){
+            System.out.println("El resultado del problema (4 + 6 == 10 || 2 == 3) &&! (8 < 8 && 3>2) || 7% 2 == 1 es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema (4 + 6 == 10 || 2 == 3) &&! (8 < 8 && 3>2) || 7% 2 == 1 es FALSO");
+        }
+
+        //Problema 17
+        if (((12 / 3 > 4) && (5 == 5 || 10 != 10) && ! (8 >= 9))==true){
+            System.out.println("El resultado del problema (12 / 3 > 4) && (5 == 5 || 10 != 10) && ! (8 >= 9) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema (12 / 3 > 4) && (5 == 5 || 10 != 10) && ! (8 >= 9) es FALSO");
+        }
+
+        //problema 18
+        if ((!(7+3<10)&&(9/3==3||6<2)&&(5*4>=20))==true){
+            System.out.println("El resultado del problema !(7+3<10)&&(9/3==3||6<2)&&(5*4>=20) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema !(7+3<10)&&(9/3==3||6<2)&&(5*4>=20) es FALSO");
+        }
+
+        //Problema 19
+        if ((((10 - 2) != 8) && !(6 >= 7 || 2 == 2) && (12 % 3 == 0))==true){
+            System.out.println("El resultado del problema ((10 - 2) != 8) && !(6 >= 7 || 2 == 2) && (12 % 3 == 0) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema ((10 - 2) != 8) && !(6 >= 7 || 2 == 2) && (12 % 3 == 0) es FALSO");
+        }
+
+        //Problema 20
+        if ((!(15 > 5 && 3 + 2 == 5) || (9 < 10 && !(6 <= 4)) && (7 * 2 != 14))==true){
+            System.out.println("El resultado del problema !(15 > 5 && 3 + 2 == 5) || (9 < 10 &&! (6 <= 4)) && (7 * 2 != 14) es VERDADERO");
+        } else {
+            System.out.println("El resultado del problema !(15 > 5 && 3 + 2 == 5) || (9 < 10 &&! (6 <= 4)) && (7 * 2 != 14) es FALSO");
+        }
+        //FIN
+    }
+
+}
