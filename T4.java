a8a89c618469311af3c6a2d8dbe448581b442b84
xLeoHurt <leohurt06@gmail.com>
2024-11-11 12:40:56 -0600

    T4

---
+++ T4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class T4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("ingresa la cantidad de numeros a evaluar:" );
+        int numeros = input.nextInt();
+
+        int pares = 0;
+        int impares = 0;
+
+        for (int contador = 0; contador < numeros; contador++) {
+            System.out.println("Ingrese el numero " + (contador + 1 ) + ":" );
+            int numero = input.nextInt();
+
+            if (numero % 2 == 0) {
+                pares++;
+            } else {
+                impares++;
+            }
+        }
+        System.out.println("Cantidad de pares: " + pares);
+        System.out.println("Cantidad de impares: " + impares);
+    }
+}
